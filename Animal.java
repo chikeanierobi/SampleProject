df4c2ce9bf796b758234f4cb86fe645541ca5df1
ckaniero <95322855+chikeanierobi@users.noreply.github.com>
2022-05-24 20:52:45 -0400

    Create Animal.java
    Modified the Animal Class to just have 2 functions instead.

---
+++ Animal.java
@@ -0,0 +1,32 @@
+public class Animal {
+    private String type;
+    private char size;
+    private int age;
+    private String color;
+
+    public void setInfo(String type, char size, int age, String color){
+        this.type = type;
+        this.size = size;
+        this.age = age;
+        this.color = color;
+    }
+
+    public void printInfo(){
+        System.out.println("This animal is a " + age + "-year-old " +  type + " coloured " + color +  " of size " + size + ".");
+    }
+    
+    
+    public static void main(String[] args){
+        Animal Cat = new Animal();
+        Animal Dog = new Animal();
+        Animal Mouse = new Animal();
+        Cat.setInfo("Cat",'M',12,"Orange");
+        Dog.setInfo("Dog",'L',33,"Beige");
+        Mouse.setInfo("Mouse",'S',6,"White");
+        Cat.printInfo();
+        Dog.printInfo();
+        Mouse.printInfo();
+    }
+}
+
+

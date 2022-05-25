6c2407e8b8b813b6171e1a4c03459ac5a0260231
ckaniero <95322855+chikeanierobi@users.noreply.github.com>
2022-05-24 21:11:14 -0400

    Update Animal.java
    Modified the Animal class to just have 2 functions

--- Animal.java
+++ Animal.java
@@ -4,71 +4,28 @@ public class Animal {
     private int age;
     private String color;
 
-    public void setType(String type){
+    public void setInfo(String type, char size, int age, String color){
         this.type = type;
-    }
-
-    public String getType(){
-        return type;
-    }
-
-    public void setSize(char size){
         this.size = size;
-    }
-
-    public char getSize(){
-        return size;
-    }
-
-    public void setAge(int age){
         this.age = age;
-    }
-
-    public int getAge(){
-        return age;
-    }
-
-    public void setColor(String color){
         this.color = color;
     }
 
-    public String getColor(){
-        return color;
+    public void printInfo(){
+        System.out.println("This animal is a " + age + "-year-old " +  type + " coloured " + color +  " of size " + size + ".");
     }
+    
     
     public static void main(String[] args){
         Animal Cat = new Animal();
         Animal Dog = new Animal();
         Animal Mouse = new Animal();
-        Cat.setType("Cat");
-        Dog.setType("Dog");
-        Mouse.setType("Mouse");
-        Cat.setAge(12);
-        Mouse.setAge(22);
-        Dog.setAge(33);
-        Cat.setColor("Orange");
-        Dog.setColor("Beige");
-        Mouse.setColor("Grey");
-        Cat.setSize('M');
-        Dog.setSize('L');
-        Mouse.setSize('S');
-        System.out.println("ANIMAL 1");
-        System.out.println("Type: " + Cat.getType());
-        System.out.println("Size: " + Cat.getSize());
-        System.out.println("Age: " + Cat.getAge());
-        System.out.println("Color: " + Cat.getColor());
-        System.out.println("-------------------------------------------------------------------");
-        System.out.println("ANIMAL 2");
-        System.out.println("Type: " + Dog.getType());
-        System.out.println("Size: " + Dog.getSize());
-        System.out.println("Age: " + Dog.getAge());
-        System.out.println("Color: " + Dog.getColor());
-        System.out.println("------------------------------------------------------------------");
-        System.out.println("ANIMAL 3");
-        System.out.println("Type: " + Mouse.getType());
-        System.out.println("Size: " + Mouse.getSize());
-        System.out.println("Age: " + Mouse.getAge());
-        System.out.println("Color: " + Mouse.getColor());
+        Cat.setInfo("Cat",'M',12,"Orange");
+        Dog.setInfo("Dog",'L',33,"Beige");
+        Mouse.setInfo("Mouse",'S',6,"White");
+        Cat.printInfo();
+        Dog.printInfo();
+        Mouse.printInfo();
     }
 }
 
